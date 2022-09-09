8eb3efa63193ba803de034e7f7ea6caca0318cc8
barra <vincent.barra@uca.fr>
2022-09-09 13:20:15 +0200

    changes

--- _build/jupyter_execute/Stats.py
+++ _build/jupyter_execute/Stats.py
@@ -3,11 +3,18 @@
 
 # # Introduction aux statistiques
 # 
-# ## Quelques exemples
-# ````{prf:example}
+# ## Vocabulaire et définition
+# On définit ici de manière informelle les termes utilisés dans la suite :
+# - **Population** : ensemble de cardinalité finie, notée $N$, ou infinie
+# - **Echantillon** : sous-ensemble de la population, de cardinalité $n$
+# - **Individu** : sous-ensemble de la population ou de l'échantillon, de cardinalité 1
+# - **Caractère** : nature de la caractéristique à laquelle on s'intéresse statistiquement. Il peut être **qualitatif** (nominal ou ordinal) ou **quantitatif**(discret ou continu)
 # 
-# ````
+# ## Démarche générale
 # 
-# ````{prf:example}
+# ## Probabilités et statistiques
+# La question qui se pose est la suivante : comment définir ou estimer la valeur de probabilité associée àun  évènement ?
+# Plusieurs points de vue ont été proposés et adoptés que synthétisons très brièvement.
 # 
-# ````
+# ### Approche fréquentiste
+# Ce point de vue historique, souvent présenté comme le plus “naturel” ou “objectif”, consiste à définir une probabilité comme la limite de la fréquence empirique lorsque la taille de l'echantillon devient infinie
