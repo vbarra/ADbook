424f09cb751f55ece0fb99a10dee0ed9c9377b70
barra <vincent.barra@uca.fr>
2022-10-20 14:37:52 +0200

    changes

--- _build/jupyter_execute/statsexploratoires.py
+++ _build/jupyter_execute/statsexploratoires.py
@@ -879,7 +879,11 @@ print (print_tab (n, p, ind, cosinus_carres))
 # 
 # 
 # On représente alors graphiquement les individus ligne et colonne sur le premier plan factoriel.
+# |  |  |
+# 
 # | ![](./images/planlig.png) | ![](./images/plancol.png) |
+# |------------------------------------------------------------|----------------------------------------------------------------|
+# | Individus ligne  | Individus colonne   |
 # 
 # 
 # 
