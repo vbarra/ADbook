67c4fc5f86f4d0f7b2cc0199d911f3fdd4eec2d3
barra <vincent.barra@uca.fr>
2023-06-08 16:20:45 +0200

    changes

--- _build/jupyter_execute/Rappels1.py
+++ _build/jupyter_execute/Rappels1.py
@@ -90,6 +90,7 @@
 # 
 # $(n-1)\mathbb{E}({S'}_n^2)=\displaystyle\sum_{k=1}^n\mathbb{E}(X_k^2)-n\mathbb{E}(\bar{X_n^2})=n(\sigma^2+m^2)-n\left (\frac{\sigma^2}{n}+m^2 \right )$
 # et le résultat.
+# 
 # 3. Admis
 # 
 # 
